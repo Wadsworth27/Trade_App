e1466cd4496a965ca1998d5f22746bcdf88f8b7d
Steven Wadsworth <Stwadworth27@gmail.com>
2023-06-25 00:07:48 -0400

    Update trade_app.py

--- trade_app.py
+++ trade_app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import json
 import streamlit as st
+import plotly.express as px
 from framebuilder import FrameBuilder
 
 import warnings
@@ -34,11 +35,40 @@ def load_data():
 
 data = load_data()
 
-st.title("SOTH Trade App")
+st.title("Chad's SOTH Trade App")
 
 min_year_filter, max_year_filter = st.slider("Year", 2023, 2050, (2023, 2027))
 
+with st.sidebar:
+    options = st.multiselect(
+        "Player Select:",
+        options=data["Original Owner"].unique(),
+        default=data["Original Owner"].unique(),
+    )
+
 st.subheader("Raw data")
-st.write(
-    data.loc[(data["Season"] >= min_year_filter) & (data["Season"] <= max_year_filter)]
+working_data = data.loc[
+    (data["Season"] >= min_year_filter)
+    & (data["Season"] <= max_year_filter)
+    & (data["Pick Owner"].isin(options))
+]
+# st.write(working_data)
+# Display Bar Graph of picks in range
+bar_data = (
+    working_data[(~working_data["Pick Lost"]) & (working_data["Pick Round"] < 8)]
+    .groupby(["Pick Round", "Pick Owner"])
+    .count()
+    .reset_index()
 )
+bar_data.rename({"Original Owner": "# of Picks"}, axis=1, inplace=True)
+fig = px.bar(
+    bar_data,
+    x="Pick Round",
+    y="# of Picks",
+    color="Pick Owner",
+)
+st.plotly_chart(fig, use_container_width=True)
+print(bar_data)
+
+
+print(options)
